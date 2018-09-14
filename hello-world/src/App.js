d25431552ebf7eebc0fb0852a615fa6431f79b81
Dmiyamoto <m2985.damiyamoto@gmail.com>
2018-09-14 23:23:58 +0900

    learn jsx syntax

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,8 +1,44 @@
 import React, { Component } from 'react';
 
+// class App extends Component {
+//   render() {
+//     return <div>Hello, world!</div>;
+//   }
+// }
+
+// //上記がトランスパイルされると、以下のようなコードが表示される。
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello, world!!"
+//     );
+//   }
+// }
+
+// class App extends Component {
+//   render() {
+//     return <h1>Hello, world!</h1>;
+//   }
+// }
+
+// class App extends Component {
+//   render() {
+//     const greeting = "Hi, Tom!";
+//     const dom = <h1 className="foo">{greeting}</h1>; 
+//     return dom;
+//   }
+// }
+
 class App extends Component {
   render() {
-    return <div><h1>Hello, world!</h1></div>;
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked.")}} />
+      </React.Fragment>
+    )
   }
 }
 
